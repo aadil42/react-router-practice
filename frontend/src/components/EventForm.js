e3995c8b132a98fd73c893124f533f7033c2ad09
aadil42 <adilkelawala@gmail.com>
2023-06-15 13:01:38 +0530

    displaying errors when the input value is invalid.

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,10 +1,12 @@
-import { useNavigate, Form, useNavigation } from 'react-router-dom';
+import { useNavigate, Form, useNavigation, useActionData } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
   const navigation = useNavigation();
+
+  const errorData = useActionData();
 
   const isSubmitting = navigation.state === 'submitting';
 
@@ -14,6 +16,13 @@ function EventForm({ method, event }) {
 
   return (
     <Form method='post' className={classes.form}>
+      {errorData && errorData.errors && 
+        <ul>
+          {Object.values(errorData.errors).map((error) => {
+           return <li key={error}>{error}</li>;
+          })}
+        </ul>
+      }
       <p>
         <label htmlFor="title">Title</label>
         <input id="title" type="text" name="title" required defaultValue={event && event.title}/>
